9233b0cafa9d51b819dc709a7cc19c7a1cc7031c
EugeneDnipro <EugeneDnepr@gmail.com>
2023-02-02 14:57:41 +0200

    Removed psvm

--- src/main/java/org/example/SumCalculator.java
+++ src/main/java/org/example/SumCalculator.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class SumCalculator {
-    public int sum(int n) throws IllegalArgumentException{
+    public int sum(int n) throws IllegalArgumentException {
         if (n == 0) {
             throw new IllegalArgumentException("Illegal index: " + n);
         }
@@ -11,10 +11,5 @@ public class SumCalculator {
             res += i;
         }
         return res;
-    }
-
-    public static void main(String[] args) {
-        SumCalculator calc = new SumCalculator();
-        calc.sum(0);
     }
 }

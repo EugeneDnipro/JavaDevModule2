7752266a5a6a9d2148e8b039bb3d96dfe646b637
EugeneDnipro <EugeneDnepr@gmail.com>
2023-02-09 12:06:09 +0200

    Stream applied

--- src/main/java/org/example/SumCalculator.java
+++ src/main/java/org/example/SumCalculator.java
@@ -1,15 +1,12 @@
 package org.example;
 
+import java.util.stream.IntStream;
+
 public class SumCalculator {
     public int sum(int n) throws IllegalArgumentException {
         if (n == 0) {
             throw new IllegalArgumentException("Illegal index: " + n);
         }
-
-        int res = 0;
-        for (int i = 1; i <= n; i++) {
-            res += i;
-        }
-        return res;
+        return IntStream.rangeClosed(1, n).sum();
     }
 }
